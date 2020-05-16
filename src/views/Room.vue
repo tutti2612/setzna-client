052b8560f7bc9f7c45778d6a0a28f64e58e50d7d
Kohei Tsuchiya <kohei@KoheinoMacBook-Air.local>
2020-05-16 14:59:16 +0900

    setzna-awsと連携した

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -2,7 +2,7 @@
   <v-app id="room">
     <v-content class="grey lighten-4 pt-0">
       <v-container class="cardContainer" fluid>
-        <div v-for="message in messages" :key="message.name">
+        <div v-for="message in messages" :key="message.createdAt">
           <Card v-bind="message" />
         </div>
       </v-container>
@@ -18,7 +18,7 @@
             small
             color="primary"
             @click="submit"
-            :disabled="content === ''"
+            :disabled="content === '' && !isWsOpened"
           >
             <v-icon dark>mdi-pencil</v-icon>
           </v-btn>
@@ -35,6 +35,7 @@ import Card from "@/components/Card.vue";
 interface Message {
   name: string;
   content: string;
+  createdAt: string;
 }
 
 export default Vue.extend({
@@ -43,16 +44,51 @@ export default Vue.extend({
     Card
   },
   props: {
-    name: { type: String }
+    name: { type: String, default: "John Doe" }
   },
   data: () => ({
     content: "",
-    messages: [] as Message[]
+    messages: [] as Message[],
+    latitude: 123.1234, //TODO: 端末の緯度を取得する
+    longitude: 65.4567, //TODO: 端末の経度を取得する
+    ws: {} as WebSocket,
+    isWsOpened: false
   }),
+  created: function() {
+    this.ws = new WebSocket(
+      `wss://g9nun1ag6h.execute-api.ap-northeast-1.amazonaws.com/Prod?latitude=${this.latitude}&longitude=${this.longitude}`
+    );
+    this.ws.onopen = () => {
+      console.log("WebSocket has been opened.");
+      this.isWsOpened = true;
+    };
+    this.ws.onmessage = msg => {
+      console.log("Received a message.");
+      console.log(msg);
+      this.messages.push(JSON.parse(msg.data));
+    };
+    this.ws.onerror = e => {
+      console.error("WebSocket error observed:", e);
+    };
+  },
+  beforeDestroy: function() {
+    this.ws.close();
+    console.log("WebSocket has been closed.");
+  },
   methods: {
     submit() {
-      this.messages.push({ name: this.name, content: this.content });
+      // this.messages.push({ name: this.name, content: this.content });
+      const sendData = {
+        message: "sendmessage",
+        data: {
+          name: this.name,
+          content: this.content,
+          createdAt: Date.now()
+        }
+      };
+      this.ws.send(JSON.stringify(sendData));
       this.content = "";
+      console.log("Sent a messege.");
     }
   }
 });
